88a2b9a546f257aac4dcb06d571ed3e190ca75e1
zakaria552 <zakaria.farah55@outlook.com>
2022-11-14 21:02:44 +0000

    mvc design pattern setup

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,6 +1,13 @@
-const {selectTopics, selectArticles} = require("../models/articles.model")
+const {selectTopics, selectArticles, selectCommentsByArticleid} = require("../models/articles.model")
 exports.getArticles = (req, res) => {
     return selectArticles().then((articles) => {
         res.status(200).send(articles)
+    })
+}
+exports.getCommentsByArticleid = (req, res, next) => {
+    selectCommentsByArticleid(req.params.article_id).then((comments) => {
+        res.status(200).send(comments)
+    }).catch((err) => {
+        next(err)
     })
 }
--- models/articles.model.js
+++ models/articles.model.js
@@ -12,3 +12,13 @@ exports.selectArticles = () => {
         return results.rows
     })
 }
+exports.selectCommentsByArticleid = (article_id) => {
+    if(isNaN(parseInt(article_id))) return Promise.reject({status: 400, msg: "bad request!"})
+    const queryStr = `SELECT comment_id, votes, created_at, author,
+    body FROM comments WHERE article_id = $1;
+    `
+    return db.query(queryStr, [article_id]).then((results) => {
+        if(results.rows.length === 0) return Promise.reject({status: 404, msg: "article not found!"})
+        return results.rows
+    })
+}
