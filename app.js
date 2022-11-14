1f6a04fa647b99e089aab3cb6b11c3b4b0791e4c
zakaria552 <zakaria.farah55@outlook.com>
2022-11-14 21:04:19 +0000

    get comments by article id and error handeled

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const app = express()
 const {getTopics} = require("./controllers/topics.controller")
-const {getArticles} = require("./controllers/articles.controller")
+const {getArticles, getCommentsByArticleid} = require("./controllers/articles.controller")
 
 app.get("/api/topics", getTopics)
 app.get("/api/articles", getArticles)
-
+app.get("/api/articles/:article_id/comments", getCommentsByArticleid)
 app.all("/*", (req, res) => {
     res.status(404).send({msg: "route not found!"})
+})
+
+app.use((err, req, res, next) => {
+    res.status(err.status).send({msg: err.msg})
 })
 module.exports = app
