5c1461f7eb42de4fc33834c97f47998b6f59aec7
zakaria552 <zakaria.farah55@outlook.com>
2022-11-15 14:27:00 +0000

    removed console log and ordered the comments in descending order

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,7 +1,6 @@
 const {selectCommentsByArticleid} = require("../models/comments.model")
 exports.getCommentsByArticleid = (req, res, next) => {
     selectCommentsByArticleid(req.params.article_id).then((comments) => {
-        console.log(comments)
         res.status(200).send({comments})
     }).catch((err) => {
         next(err)
--- models/comments.model.js
+++ models/comments.model.js
@@ -3,11 +3,10 @@ const db = require("../db/connection")
 exports.selectCommentsByArticleid = (article_id) => {
     return selectArticleById(article_id).then(() => {
         const queryStr = `SELECT comment_id, votes, created_at, author,
-        body FROM comments WHERE article_id = $1;
+        body FROM comments WHERE article_id = $1 ORDER BY comment_id DESC;
         `
         return db.query(queryStr, [article_id])
     }).then((results) => {
-        console.log(results.rows)
         return results.rows
     })
 }
