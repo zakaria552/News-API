29c0dc957d5523bcce54b8e9b7e70fca844353bd
zakaria552 <zakaria.farah55@outlook.com>
2022-11-15 11:20:38 +0000

    endpoint set and test suites

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,3 +46,29 @@ describe("/api/articles", () => {
         })
     })
 })
+describe("api/articles/:article_id", () => {
+    test(":) GET 200 - returns article given the article id", () => {
+        return request(app).get("/api/articles/3").expect(200).then(({body}) => {
+            expect(Object.keys(body).length).toBe(7)
+            expect(body).toEqual({
+                "author" : expect.any(String),
+                "title": expect.any(String),
+                "article_id" : 3,
+                "body" : expect.any(String),
+                "topic": expect.any(String),
+                "created_at": expect.any(String),
+                "votes" : expect.any(Number)
+            })
+        })
+    })
+    test(":( GET 400 - returns bad request given invalid ID", () => {
+        return request(app).get("/api/articles/dfs").expect(400).then(({body}) => {
+            console.log(body)
+        })
+    })
+    test(":( GET 404 - returns bad request given non-existent article_id", () => {
+        return request(app).get("/api/articles/300").expect(404).then(({body}) => {
+            expect(body.msg).toEqual("article not found!")
+        })
+    })
+})
