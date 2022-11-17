d8fa9b53dbeda8d6cc42f3f52bf55c874ada5809
zakaria552 <zakaria.farah55@outlook.com>
2022-11-17 11:52:04 +0000

    mvc pattern setup

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -1,4 +1,4 @@
-const {selectCommentsByArticleid, createCommentByArticleid, removeComment} = require("../models/comments.model")
+const {selectCommentsByArticleid, createCommentByArticleid, removeComment, updateComment} = require("../models/comments.model")
 exports.getCommentsByArticleid = (req, res, next) => {
     selectCommentsByArticleid(req.params.article_id).then((comments) => {
         res.status(200).send({comments})
@@ -19,6 +19,14 @@ exports.deleteComment = (req, res, next) => {
     removeComment(req.params.comment_id).then(() => {
         res.status(204).send({})
     }).catch((err) => {
+        next(err)
+    })
+}
+exports.patchComment = (req, res, next) => {
+    updateComment(req.params.comment_id, req.body).then((comment) => {
+        res.status(200).send({comment})
+    }).catch((err) => {
+        console.log(err)
         next(err)
     })
 }
