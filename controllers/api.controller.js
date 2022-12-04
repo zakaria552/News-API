8affc79c92c9de52dd621c827ac6e11adbcee54e
zakaria552 <zakaria.farah55@outlook.com>
2022-12-04 17:35:03 +0000

    data is parsed

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,6 +1,6 @@
 const fs = require("fs/promises")
 exports.getApi = (req, res, next) => {
     fs.readFile("endpoints2.json").then((content) => {
-        res.status(200).send({endpoints: content})
+        res.status(200).send(JSON.parse(content))
     })
 }
